2a9748c670c4b16db2d606fa227ed807f101472c
poorvasha <poorvashadmp@gmail.com>
2020-06-17 11:32:35 +0530

    [IMP] Sample_DB_conc : data inserted in database.
    Data insert code is added in java class file.

--- src/main/java/com/mycompany/sample_db_conc/Database.java
+++ src/main/java/com/mycompany/sample_db_conc/Database.java
@@ -12,6 +12,18 @@ public class Database
             Connection con=DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/testDemo","root","");
             System.out.println("success");
+            
+            String sql = "INSERT INTO student (ID, Name, Phn_no) VALUES (?, ?, ?)";
+ 
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, "0");
+            statement.setString(2, "Yuva");
+            statement.setString(3, "9090909090");
+ 
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new user was inserted successfully!");
+}
         }
         catch(Exception e){
            System.out.println(e);
